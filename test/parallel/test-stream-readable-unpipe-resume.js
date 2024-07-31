620431c5ff2d9437a6c657a64ca9e54639eee4cd
Pietro Marchini <pietro.marchini94@gmail.com>
2024-07-31 17:30:52 +0200

    test: flaky test-stream-readable-unpipe-resume

--- test/parallel/test-stream-readable-unpipe-resume.js
+++ test/parallel/test-stream-readable-unpipe-resume.js
@@ -4,7 +4,10 @@ const common = require('../common');
 const stream = require('stream');
 const fs = require('fs');
 
-const readStream = fs.createReadStream(process.execPath);
+const fixtures = require('../common/fixtures');
+const fixtureFilePath = fixtures.path('x.txt');
+
+const readStream = fs.createReadStream(fixtureFilePath);
 
 const transformStream = new stream.Transform({
   transform: common.mustCall(() => {
